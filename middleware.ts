3abc4bf997a6d11fb3a9fdb991482e48acaa6494
Pkb2311 <100441423+Pkb2311@users.noreply.github.com>
2026-01-09 20:19:11 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -29,9 +29,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/admin/:path*"],
-};
-
-export const config = {
   matcher: ["/admin/:path*", "/api/admin/:path*"],
 };
