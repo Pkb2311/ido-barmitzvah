f701f44694dd1c7598c6022bdd6249924184320c
Pkb2311 <100441423+Pkb2311@users.noreply.github.com>
2026-01-12 20:20:20 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
+
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
