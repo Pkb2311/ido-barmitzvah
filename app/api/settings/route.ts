bbf32d51a90e87527878fea9178ddfe003a7fd03
Pkb2311 <100441423+Pkb2311@users.noreply.github.com>
2026-01-14 21:03:13 +0200

    Add 'must-revalidate' to Cache-Control header

--- app/api/settings/route.ts
+++ app/api/settings/route.ts
@@ -74,7 +74,7 @@ export async function GET() {
     {
       status: 200,
       headers: {
-        "Cache-Control": "no-store, max-age=0",
+          "Cache-Control": "no-store, max-age=0, must-revalidate",
       },
     }
   );
